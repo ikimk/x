7e551c7022e87e9d0e9f5bc7779f0aef3f91190e
Ayush Shukla <60759495+ikimk@users.noreply.github.com>
2021-04-24 04:09:22 +0530

    x

--- JS/App.js
+++ JS/App.js
@@ -3,28 +3,16 @@ gsap.registerPlugin(ScrollTrigger);
 //---------------------Landing Page ScrollTrigger---------------------
 function LandingPageScrollTrigger() {
 
-    gsap.to('body', { // LoadingAnimation---------------------
-        opacity: 1, duration: 0,
-    }) // /LoadingAnimation---------------------
+    //Elements
+    var card = document.querySelector("#card");
 
-    let LandingPageScrollTrigger = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".ImgWrapper",
-            start: "0% 0%",
-            end: "100% 0%",
-            pin: ".ImgWrapper",
-            scrub: 2,
-            // markers: "true",
-        }
-    })
-    LandingPageScrollTrigger
-        .to('#wrapper #img7', { transform: 'scale(3)', x: "100%", }, 0)
-        .to('#wrapper #img6', { transform: 'scale(2.5)', x: "-125%" }, 0)
-        .to('#wrapper #img5', { transform: 'scale(1.7)', x: "-35%", y: "-15%", }, 0)
-        .to('#wrapper #img4', { transform: 'scale(1.5)', x: "-60%", y: "-30%", }, 0)
-        .to('#wrapper #img3', { transform: 'scale(1.5)', x: "10%", y: "-10%", }, 0)
-        .to('#wrapper #img2', { transform: 'scale(1.4)', x: "15%" }, 0)
-        .to('#wrapper #img1', { transform: 'scale(1.3)', x: "-25%" }, 0)
+    //MouseMove Event
+    $('#wrapper').mousemove(function (e) {
+        let x = ((-113 * (window.innerWidth / 2 - e.pageX)) / window.innerWidth / 2);
+        let y = ((113 * (window.innerHeight / 2 - e.pageY)) / window.innerHeight / 2);
+
+        card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    });
 }
 //---------------------/Landing Page ScrollTrigger---------------------
 
