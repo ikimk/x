94c1e415a400c22f8217637b1050ab5e3e3aea41
Ayush Shukla <60759495+ikimk@users.noreply.github.com>
2021-05-15 20:46:41 +0530

    x

--- JS/App.js
+++ JS/App.js
@@ -15,36 +15,11 @@ function Parallax() {
         .from('footer', {
             y: "-50%", ease: "none",
         }, 0)
-        .from('footer section', {
-            backgroundSize: "140vw", ease: "none",
-        }, 0)
 }
 //---------------------/Parallax---------------------
 
-//---------------------Parallax (max-width: 480px)---------------------
-function ParallaxMobile() {
-    let tlB = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".SectionB",
-            start: "100% 100%",
-            end: () => `+=${document.querySelector("footer").offsetHeight}`,
-            scrub: 0,
-            // markers: true,
-        }
-    })
-    tlB
-        .from('footer', {
-            y: "-50%", ease: "none",
-        }, 0)
-        .from('footer section', {
-            backgroundSize: "140vw", ease: "none",
-        }, 0)
-}
-//---------------------/Parallax (max-width: 480px)---------------------
-
 window.onload = () => {
     if (window.matchMedia("(max-width: 480px)").matches) {
-        ParallaxMobile()
     } else {
         Parallax()
     }
