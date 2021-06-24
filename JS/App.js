bc283cf14884c255bbcc1d646fa77570768f10fe
Ayush Shukla <60759495+ikimk@users.noreply.github.com>
2021-06-24 14:43:48 +0530

    x

--- JS/App.js
+++ JS/App.js
@@ -1,11 +1,20 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, Draggable);
 
-document.documentElement.style.setProperty('--ColorLight1', "#0d0d0d");
-document.documentElement.style.setProperty('--ColorLight2', "#121212");
-document.documentElement.style.setProperty('--ColorLight3', "#1f1f1f");
-document.documentElement.style.setProperty('--ColorDark3', "#e3e3e3");
-document.documentElement.style.setProperty('--ColorDark2', "#ebebeb");
-document.documentElement.style.setProperty('--ColorDark1', "#f2f2f2");
+$("body").dblclick(function () {
+    document.documentElement.style.setProperty('--ColorLight1', "#0d0d0d");
+    document.documentElement.style.setProperty('--ColorLight2', "#121212");
+    document.documentElement.style.setProperty('--ColorLight3', "#1f1f1f");
+    document.documentElement.style.setProperty('--ColorDark3', "#e3e3e3");
+    document.documentElement.style.setProperty('--ColorDark2', "#ebebeb");
+    document.documentElement.style.setProperty('--ColorDark1', "#f2f2f2");
+    $("#HomePage").css({ 'background': 'url(./img/HiThereWhite.jpg) var(--ColorDark2) no-repeat center', 'backgroundSize': '100vw', });
+    $("#MyWorks").css({ 'background': 'url(./img/MyWorksWhite.jpg) var(--ColorDark2) no-repeat center', 'backgroundSize': '100vw', });
+    $("#ContactMe").css({ 'background': 'url(./img/ContactMeWhite.jpg) var(--ColorDark2) no-repeat center', 'backgroundSize': '100vw', });
+    $("#FiverrIcon img").attr("src", "Icon/FiverrBlack.svg");
+    $("#BehanceIcon img").attr("src", "Icon/BehanceBlack.svg");
+    $("#CodepenIcon img").attr("src", "Icon/CodepenBlack.svg");
+    $("#GithubIcon img").attr("src", "Icon/GithubBlack.svg");
+});
 
 //---------------------App---------------------
 function App() {
@@ -14,26 +23,69 @@ function App() {
         opacity: 1, duration: 0,
     }) // /LoadingAnimation---------------------
 
-    let tl = gsap.timeline({
+    //---------------------HomePage---------------------
+    // HomePageParallax---------------------
+    let HomePageParallax = gsap.timeline({
         scrollTrigger: {
             trigger: "#HomePage",
             start: "0% 0%",
             end: "100% 0%",
-            scroller: "#wrap",
             scrub: 0,
             // markers: true,
         }
     })
-    tl
+    HomePageParallax
         .to('#HomePage>*', {
             y: "-31%", ease: "none",
         }, 0)
         .to('#HomePage', {
             backgroundSize: '90vw', ease: "none",
         }, 0)
-        .from('#wrapX', {
+        .from('#MyWorks', {
             y: "-50%", backgroundSize: '90vw', ease: "none",
         }, 0)
+    // /HomePageParallax---------------------
+    //---------------------/HomePage---------------------
+
+    //---------------------MyWorks---------------------
+    // MyWorksDraggableTL---------------------
+    Draggable.create("#ProjectWrap", {
+        onPress: function () { MyWorksDraggableTL.play(); },
+        onRelease: function () { MyWorksDraggableTL.reverse(); },
+        bounds: "#MyWorks",
+        type: "x",
+        // cursor: "default",
+        // activeCursor: "default",
+    });
+    var MyWorksDraggableTL = gsap.timeline();
+    MyWorksDraggableTL.to('#ProjectSlide>*', {
+        y: "-10%", duration: 0.2,
+    }, 0)
+    MyWorksDraggableTL.pause();
+    // /MyWorksDraggableTL---------------------
+
+    // MyWorksParallax---------------------
+    let MyWorksParallax = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#MyWorks",
+            start: "50% 0%",
+            end: "150% 0%",
+            scrub: 0,
+            // markers: true,
+        }
+    })
+    MyWorksParallax
+        .to('#MyWorks>*', {
+            y: "-31%", ease: "none",
+        }, 0)
+        .to('#MyWorks', {
+            backgroundSize: '90vw', ease: "none",
+        }, 0)
+        .from('#ContactMe', {
+            y: "-50%", backgroundSize: '90vw', ease: "none",
+        }, 0)
+    // /MyWorksParallax---------------------
+    //---------------------/MyWorks---------------------
 }
 //---------------------/App---------------------
 
