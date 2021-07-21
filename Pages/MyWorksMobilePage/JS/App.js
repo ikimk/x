19dd639cba7583ee27905ad6fad5b29934c3eb70
Ayush Shukla <60759495+ikimk@users.noreply.github.com>
2021-07-22 04:04:37 +0530

    x

--- Pages/MyWorksMobilePage/JS/App.js
+++ Pages/MyWorksMobilePage/JS/App.js
@@ -2,21 +2,37 @@ gsap.registerPlugin(ScrollTrigger);
 
 //---------------------PageAnimation---------------------
 function PageAnimation() {
-    //---------------------MyWorks---------------------
-    // MyWorksTL---------------------
-    let MyWorksTL = gsap.timeline({ defaults: { ease: "power3.out", duration: 1.3, } });
-    MyWorksTL
-        .from("#MyWorks #MyWorksTitle h1 .word", {
+    //---------------------MyWorksX---------------------
+    // MyWorksXTL---------------------
+    let MyWorksXTL = gsap.timeline({
+        defaults: { ease: "power3.out", duration: 1.3, },
+        scrollTrigger: {
+            trigger: "#HomePage",
+            toggleActions: "play reverse play reverse",
+            onToggle: self => self.animation.timeScale(self.isActive ? 1 : -1.3),
+            start: "0% 13%",
+            end: "100% 13%",
+            // markers: "true",
+        }
+    })
+    MyWorksXTL
+        .from("#MyWorksX #MyWorksXTitle h1 .word", {
             yPercent: 100, stagger: 0.13,
         }, 0)
-        .from("#MyWorks #ProjectSlide #ProjectTitle h4 .word", {
+        .from("#MyWorksX #MyWorksXTitle", {
+            yPercent: 82,
+        }, 0)
+        .from("#MyWorksX #ProjectSlide", {
+            yPercent: 220, stagger: 0.13,
+        }, 0)
+        .from("#MyWorksX #ProjectSlide #ProjectTitle h4 .word", {
             yPercent: 100, stagger: 0.061,
         }, 0.31)
-        .from("#MyWorks #ProjectSlide #ProjectTitle h2 .char", {
+        .from("#MyWorksX #ProjectSlide #ProjectTitle h2 .char", {
             yPercent: 100, stagger: 0.031,
         }, 0.31)
-    // /MyWorksTL---------------------
-    //---------------------/MyWorks---------------------
+    // /MyWorksXTL---------------------
+    //---------------------/MyWorksX---------------------
 }
 //---------------------/PageAnimation---------------------
 
